a71303c397cb8a294965f72ab1c7520d19607e71
keanemu <keanemu@gmail.com>
2017-12-10 05:42:57 -0500

    commit before html modification

--- src/main/java/app/util/ResultListParser.java
+++ src/main/java/app/util/ResultListParser.java
@@ -11,63 +11,68 @@ import java.util.HashMap;
 
 import com.google.api.services.customsearch.model.Result;
 
-
 /**
  * @author Isolachine
  *
  */
 public class ResultListParser {
-    class WeightedResults implements Comparable<Object>{
-    		Result result;
+    class WeightedResults implements Comparable<Object> {
+        Result result;
         int weight;
-        WeightedResults (Result r, int w) {
-    			this.result = r;
-        		this.weight = w;
+
+        WeightedResults(Result r, int w) {
+            this.result = r;
+            this.weight = w;
         }
+
         public int compareTo(Object rhs) {
-        		WeightedResults other = (WeightedResults) rhs;
-        		int diff = weight - other.weight;
-        		if (diff == 0)
-        			return 0;
-        		else if (diff < 0)
-        			return 1;
-        		else
-        			return -1;
+            WeightedResults other = (WeightedResults) rhs;
+            int diff = weight - other.weight;
+            if (diff == 0)
+                return 0;
+            else if (diff < 0)
+                return 1;
+            else
+                return -1;
         }
     }
-    
+
     /**
      * [parse the results list and return sorted list]
-     * @param  results 
-     *             [results list from CSE]
+     * 
+     * @param results
+     *            [results list from CSE]
      * @return [sorted results list, based on original position and times of appearance]
      */
     public List<Result> parseCSEList(List<Result> results) {
-    		Map<String,WeightedResults> map = new HashMap<String,WeightedResults>();
-        for (int i = 0; i < results.size() ; i++) {
-        		Result r = results.get(i);
-        		boolean isMain = false;
-        		String u = new Util().parseUrl(r.getLink().toLowerCase());
-        		if (r.getLink().toLowerCase().equals(u))
-        			isMain = true;
-        		if (!u.equals("")) {
-        			if (isMain || !map.containsKey(u))
-        				map.put(u, new WeightedResults(r, map.getOrDefault(u, new WeightedResults(null,0)).weight+results.size()-i));
-        			else
-        				map.put(u, new WeightedResults(map.get(u).result, map.getOrDefault(u, new WeightedResults(null,0)).weight+results.size()-i));
-        		}
-        		else continue;	
+        Map<String, WeightedResults> map = new HashMap<String, WeightedResults>();
+        for (int i = 0; i < results.size(); i++) {
+            Result r = results.get(i);
+            boolean isMain = false;
+            String u = Util.parseUrl(r.getLink().toLowerCase());
+            if (r.getLink().toLowerCase().equals(u))
+                isMain = true;
+            if (!u.equals("")) {
+                if (isMain || !map.containsKey(u))
+                    map.put(u, new WeightedResults(r, map.getOrDefault(u, new WeightedResults(null, 0)).weight + results.size() - i));
+                else
+                    map.put(u, new WeightedResults(map.get(u).result, map.getOrDefault(u, new WeightedResults(null, 0)).weight + results.size() - i));
+            } else
+                continue;
         }
         List<WeightedResults> tmpList = new ArrayList<WeightedResults>(map.values());
         Collections.sort(tmpList);
         List<Result> newList = new ArrayList<Result>();
         for (WeightedResults wr : tmpList) {
-        		newList.add(wr.result);
+            wr.result.setLink(Util.parseLink(wr.result.getLink().toLowerCase()));
+            wr.result.setHtmlFormattedUrl(Util.parseUrl(wr.result.getHtmlFormattedUrl().toLowerCase()));
+            wr.result.setFormattedUrl(Util.parseUrl(wr.result.getFormattedUrl().toLowerCase()));
+            wr.result.setTitle(Util.parseTitle(wr.result.getTitle())[0] + " (" + Util.parseTitle(wr.result.getTitle())[1] + ")");
+            
+            System.out.println(wr.result.getHtmlFormattedUrl().toLowerCase());
+            newList.add(wr.result);
         }
         return newList;
     }
-    
-    
-    
-    
+
 }
--- src/main/java/app/util/Util.java
+++ src/main/java/app/util/Util.java
@@ -18,7 +18,7 @@ public class Util {
      *            [entry title in SERP, contains movie title, year, etc]
      * @return [String array, String[0] is movie title, String[1] is year]
      */
-    public String[] parseTitle(String title) {
+    public static String[] parseTitle(String title) {
         int idxYear = indexOf(Pattern.compile("\\(\\d{4}\\)"), title);
         if (idxYear < 0) {
         		int idxTypeYear = indexOf(Pattern.compile("\\([a-zA-Z\\s]+\\d{4}.*\\)"), title);
@@ -40,13 +40,29 @@ public class Util {
      *            [IMDb url in SERP]
      * @return [clean IMDb url]
      */
-    public String parseUrl(String url) {
+    public static String parseUrl(String url) {
         int idxId = indexOf(Pattern.compile("title\\/tt\\d{7}"), url);
         if (idxId < 0)
         		return "";
         else
         		return "www.imdb.com/" + url.substring(idxId, idxId + 16);
     }
+    
+    /**
+     * [parse IMDb url in google CSE SERP]
+     * 
+     * @param url
+     *            [IMDb url in SERP]
+     * @return [clean IMDb url]
+     */
+    public static String parseLink(String link) {
+        int idxId = indexOf(Pattern.compile("title\\/tt\\d{7}"), link);
+        if (idxId < 0)
+                return "";
+        else
+                return "http://www.imdb.com/" + link.substring(idxId, idxId + 16);
+    }
+    
 
     /**
      * [search for regular expression matching in given string, return the first index]
@@ -57,7 +73,7 @@ public class Util {
      *            [target string]
      * @return [index of first occurance of pattern]
      */
-    public int indexOf(Pattern pattern, String s) {
+    public static int indexOf(Pattern pattern, String s) {
         Matcher matcher = pattern.matcher(s);
         return matcher.find() ? matcher.start() : -1;
     }
